a5d55fc522e265e887534c9002131dbc6ff73cfd
Vladimir <v_korukov@hotmail.com>
2019-10-03 20:01:46 +0200

    Ran Prettier on CameraScreen.js.

--- frontend/HallucinationMachine/screens/CameraScreen.js
+++ frontend/HallucinationMachine/screens/CameraScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { 
-  Text, 
-  View, 
+import {
+  Text,
+  View,
   StyleSheet,
-  Button, 
-  Image, 
+  Button,
+  Image,
   Alert,
   CameraRoll,
   ToastAndroid,
-  Share } from 'react-native';
+  Share
+} from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 import { Ionicons } from '@expo/vector-icons';
@@ -34,106 +35,100 @@ class CameraScreen extends React.Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
 
-    FileSystem.makeDirectoryAsync(FileSystem.cacheDirectory + 'images')
-      .catch(e => {
-        console.log(e, 'Directory exists');
-      });
+    FileSystem.makeDirectoryAsync(FileSystem.cacheDirectory + 'images').catch(e => {
+      console.log(e, 'Directory exists');
+    });
   }
 
   render() {
-    const { 
-      hasCameraPermission, 
-      image, 
-      imageWidth, 
+    const {
+      hasCameraPermission,
+      image,
+      imageWidth,
       imageHeight,
       processedImage,
-      dreamButtonDisabled } = this.state;
+      dreamButtonDisabled
+    } = this.state;
     if (hasCameraPermission === null) {
       return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
-    } else if (processedImage) { 
+    } else if (processedImage) {
       return (
         <View style={styles.imageView}>
-          <Image style={styles.capturedImage} source={{uri: processedImage.uri}}/>
+          <Image style={styles.capturedImage} source={{ uri: processedImage.uri }} />
           <View style={styles.buttonBar}>
             <View>
-              <Ionicons 
-                name='md-arrow-back' 
-                size={30} 
-                onPress = {this.displayUnsavedImageAlert}
-              />
+              <Ionicons name="md-arrow-back" size={30} onPress={this.displayUnsavedImageAlert} />
             </View>
-              <Button title='Save' color='black' onPress={this.handleSaveButtonPress}/>
-            </View>
+            <Button title="Save" color="black" onPress={this.handleSaveButtonPress} />
           </View>
-      )
-    } else if (image) { // if the image is picked from the gallery or captured with the camera
+        </View>
+      );
+    } else if (image) {
+      // if the image is picked from the gallery or captured with the camera
       return (
         <View style={{ flex: 1 }}>
-          <Image style={styles.capturedImage} source={{uri: image.uri}}/>
+          <Image style={styles.capturedImage} source={{ uri: image.uri }} />
           <View style={styles.buttonBar}>
             <View>
-              <Ionicons 
-                name='md-arrow-back' 
-                size={30} 
-                onPress = {this.handleBackArrowPressed}
-              />
+              <Ionicons name="md-arrow-back" size={30} onPress={this.handleBackArrowPressed} />
             </View>
             <View>
-              <Button 
-                title='Dream' 
-                color='black' 
-                disabled={dreamButtonDisabled} 
-                onPress = {this.handleDreamButtonPress}/>
+              <Button
+                title="Dream"
+                color="black"
+                disabled={dreamButtonDisabled}
+                onPress={this.handleDreamButtonPress}
+              />
             </View>
           </View>
         </View>
-      )
-    } else if(this.state.isFocused === false) {
+      );
+    } else if (this.state.isFocused === false) {
       return (
         <NavigationEvents
-              onWillFocus={payload => {
-                  //console.log("will focus", payload);
-                  this.setState({isFocused:true})
-              }}
-              onDidBlur={payload => {
-                  //console.log('did leave',payload)
-                  this.setState({isFocused:false})
-              }}
-          />
-   ) 
-  } else { // otherwise, render the camera  
+          onWillFocus={payload => {
+            //console.log("will focus", payload);
+            this.setState({ isFocused: true });
+          }}
+          onDidBlur={payload => {
+            //console.log('did leave',payload)
+            this.setState({ isFocused: false });
+          }}
+        />
+      );
+    } else {
+      // otherwise, render the camera
       return (
         <View style={{ flex: 1 }}>
-         <Camera 
-            style={{ flex: 1 }} 
+          <Camera
+            style={{ flex: 1 }}
             type={this.state.type}
-            ref={ref => { this.camera = ref; }}>
-        <NavigationEvents
-                    onWillFocus={payload => {
-                        //console.log("will focus", payload);
-                        this.setState({isFocused:true})
-                    }}
-                    onDidBlur={payload => {
-                        //console.log('did leave',payload)
-                        this.setState({isFocused:false})
-                    }}
-          />
-          </Camera> 
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
+            <NavigationEvents
+              onWillFocus={payload => {
+                //console.log("will focus", payload);
+                this.setState({ isFocused: true });
+              }}
+              onDidBlur={payload => {
+                //console.log('did leave',payload)
+                this.setState({ isFocused: false });
+              }}
+            />
+          </Camera>
           <View style={styles.buttonBar}>
-            <View><Ionicons name='md-images' size={30} onPress={this.handlePickImage}/></View>
             <View>
-              <Ionicons 
-                name='md-camera' 
-                size={30}
-                onPress={this.handleCaptureImage}/>
+              <Ionicons name="md-images" size={30} onPress={this.handlePickImage} />
             </View>
             <View>
-              <Ionicons 
-                name='md-reverse-camera' 
-                size={30} 
-                onPress={this.handleFlipCamera}/>
+              <Ionicons name="md-camera" size={30} onPress={this.handleCaptureImage} />
+            </View>
+            <View>
+              <Ionicons name="md-reverse-camera" size={30} onPress={this.handleFlipCamera} />
             </View>
           </View>
         </View>
@@ -146,13 +141,13 @@ class CameraScreen extends React.Component {
       image: null,
       processedImage: null,
       resultSaved: false
-    })
-  }
+    });
+  };
 
   // maybe display
   displayUnsavedImageAlert = () => {
-    const {resultSaved} = this.state;
-    if(!resultSaved) {
+    const { resultSaved } = this.state;
+    if (!resultSaved) {
       Alert.alert(
         'Save the image',
         'The image will be lost if you leave this page\nWould you like to save the image?',
@@ -160,91 +155,87 @@ class CameraScreen extends React.Component {
           {
             text: 'YES',
             onPress: this.handleSaveButtonPress,
-            style: 'cancel',
+            style: 'cancel'
           },
           {
-            text: 'NO', 
+            text: 'NO',
             onPress: this.handleBackArrowPressed
-          },
+          }
         ],
-        {cancelable: false},
+        { cancelable: false }
       );
     } else {
-      this.handleBackArrowPressed()
+      this.handleBackArrowPressed();
     }
-  }
+  };
 
   handleSaveButtonPress = () => {
-    const {processedImage, resultSaved} = this.state
-    if(processedImage && !resultSaved) {
-      const path = FileSystem.cacheDirectory + 'images/processed.jpg'
-      var image_data = processedImage.uri.split('data:application/octet-stream;base64,')
-      image_data = image_data[1]
-      FileSystem.writeAsStringAsync(
-        path, 
-        image_data, 
-        {encoding: FileSystem.EncodingType.Base64})
+    const { processedImage, resultSaved } = this.state;
+    if (processedImage && !resultSaved) {
+      const path = FileSystem.cacheDirectory + 'images/processed.jpg';
+      var image_data = processedImage.uri.split('data:application/octet-stream;base64,');
+      image_data = image_data[1];
+      FileSystem.writeAsStringAsync(path, image_data, { encoding: FileSystem.EncodingType.Base64 })
         .then(res => {
-          CameraRoll.saveToCameraRoll(path)
-          .catch(err => console.log(err))
+          CameraRoll.saveToCameraRoll(path).catch(err => console.log(err));
           ToastAndroid.show('Saved', ToastAndroid.SHORT);
-          FileSystem.deleteAsync(path)
-          this.setState({resultSaved: true})
+          FileSystem.deleteAsync(path);
+          this.setState({ resultSaved: true });
         })
-        .catch(err => console.log(err))
-    } else if(!processedImage) {
-      alert('Nothing  to save!')
+        .catch(err => console.log(err));
+    } else if (!processedImage) {
+      alert('Nothing  to save!');
     }
-  }
+  };
 
   handleFlipCamera = () => {
     this.setState({
       type:
         this.state.type === Camera.Constants.Type.back
           ? Camera.Constants.Type.front
-          : Camera.Constants.Type.back,
+          : Camera.Constants.Type.back
     });
-  }
+  };
 
   handleCaptureImage = () => {
     if (this.camera) {
-      this.camera.takePictureAsync({ quality: 0.1 })
-        .then(img => this.setState({
+      this.camera.takePictureAsync({ quality: 0.1 }).then(img =>
+        this.setState({
           image: img
-        }))
+        })
+      );
     }
-  }
+  };
 
   handlePickImage = () => {
     ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: false,
       quality: 0.1
-    })
-      .then(result => {
-        if(result.cancelled === false) {
-          this.setState({
-            image: result
-          })
-        }
-      })
-  }
+    }).then(result => {
+      if (result.cancelled === false) {
+        this.setState({
+          image: result
+        });
+      }
+    });
+  };
 
   handleDreamButtonPress = () => {
-    const {image} = this.state;
-    console.log('SERVER URL: ' + SERVER_URL)
-    const selectedLayers = this.props.layers.filter(layer => layer.selected)
+    const { image } = this.state;
+    console.log('SERVER URL: ' + SERVER_URL);
+    const selectedLayers = this.props.layers.filter(layer => layer.selected);
     if (selectedLayers.length === 0) {
-      alert('No layers selected. Please go to the Settings tab and select the layers to amplify')
-      return
-    } 
-    if(image) {
-      const uri = image.uri
+      alert('No layers selected. Please go to the Settings tab and select the layers to amplify');
+      return;
+    }
+    if (image) {
+      const uri = image.uri;
       let formData = new FormData();
       let uriParts = uri.split('.');
       let fileType = uriParts[uriParts.length - 1];
 
-      this.setState({dreamButtonDisabled: true})
+      this.setState({ dreamButtonDisabled: true });
 
       formData.append('file', {
         uri,
@@ -252,24 +243,25 @@ class CameraScreen extends React.Component {
         type: `image/${fileType}`
       });
 
-      formData.append('layer', JSON.stringify(selectedLayers[0]))
+      formData.append('layer', JSON.stringify(selectedLayers[0]));
 
       let options = {
         method: 'POST',
         body: formData,
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'multipart/form-data',
+          Accept: 'application/json',
+          'Content-Type': 'multipart/form-data'
         }
       };
 
-      fetch(SERVER_URL+'/dream', options)
+      fetch(SERVER_URL + '/dream', options)
         .then(res => {
-          alert('Voilà!')
-          res.blob()
-            .then((blo) => {
+          alert('Voilà!');
+          res
+            .blob()
+            .then(blo => {
               const fr = new FileReader();
-              fr.readAsDataURL(blo)
+              fr.readAsDataURL(blo);
               fr.onload = () => {
                 const base64img = fr.result;
                 this.setState({
@@ -277,29 +269,29 @@ class CameraScreen extends React.Component {
                     uri: base64img
                   },
                   dreamButtonDisabled: false
-                })
-              }
+                });
+              };
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
         })
         .catch(err => {
-          alert('Something went wrong')
-          console.log(err)
+          alert('Something went wrong');
+          console.log(err);
           this.setState({
             dreamButtonDisabled: false
-          })
+          });
         });
     } else {
-      alert('No image picked!')
+      alert('No image picked!');
     }
-  }
+  };
 }
 
 const mapStateToProps = state => ({
   layers: state.Layers.layerData
 });
 
-export default connect(mapStateToProps)(CameraScreen)
+export default connect(mapStateToProps)(CameraScreen);
 
 const styles = StyleSheet.create({
   imageView: {
